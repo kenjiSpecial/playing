6c2655428f4b717f39ec22ed27ad739cb3a967ab
kenji <k.saito.1985@gmail.com>
2024-11-01 15:44:08 +0900

    glbloaderのエラーを解消

--- components/glb-loader.tsx
+++ components/glb-loader.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect, useRef } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
+import { Canvas } from "@react-three/fiber";
 import {
   OrbitControls,
   useGLTF,
@@ -9,8 +9,7 @@ import {
   useAnimations,
 } from "@react-three/drei";
 // import { Group } from "three";
-import { button, useControls } from "leva";
-import { AnimationAction, LoopOnce, LoopRepeat } from "three";
+import { useControls } from "leva";
 
 function Model({ url }: { url: string }) {
   // const group = useRef<Group>(null);
@@ -18,11 +17,9 @@ function Model({ url }: { url: string }) {
   const { actions, names } = useAnimations(animations, scene);
   console.log(names);
   // const actionRef = useRef<AnimationAction | null>(null);
-  const [isReversing, setIsReversing] = useState(false);
-  const targetTime = 0.5; // 停止したい時間（秒）
 
   // Levaによるアニメーションコントロール
-  const animationControls = useControls("Animation Control", {
+  useControls("Animation Control", {
     // timeline
     timeline: {
       value: 0,
@@ -39,67 +36,7 @@ function Model({ url }: { url: string }) {
         });
       },
     },
-
-    // Animation: {
-    //   options: names.reduce((acc, name) => {
-    //     acc[name] = name;
-    //     return acc;
-    //   }, {} as Record<string, string>),
-    //   value: names[0],
-    //   label: "Selected Animation",
-    // },
-    // PlaybackSpeed: {
-    //   value: 1,
-    //   min: 0.1,
-    //   max: 3,
-    //   step: 0.1,
-    //   label: "Playback Speed",
-    // },
-    // LoopPlayback: {
-    //   value: false,
-    //   label: "Loop Playback",
-    // },
-    // /** 再生ボタン */
-    // play: button(() => {
-    //   const selectedAction = actions[animationControls.Animation];
-    //   if (selectedAction) {
-    //     selectedAction.reset().play();
-
-    //     actionRef.current = selectedAction;
-    //     setIsReversing(false);
-
-    //     // selectedAction.reset();
-    //     // selectedAction.timeScale = -1;
-    //     // selectedAction.time = selectedAction.getClip().duration;
-    //     // selectedAction.play();
-    //   }
-    // }),
   });
-
-  useEffect(() => {
-    // Stop all actions
-    Object.values(actions).forEach((action) => {
-      action?.stop();
-    });
-
-    // Play the selected animation
-    const selectedAction = actions[animationControls.Animation];
-    if (selectedAction) {
-      selectedAction.setLoop(
-        animationControls.LoopPlayback ? LoopRepeat : LoopOnce, // 2: Loop, 0: No Repeat
-        Infinity
-      );
-      selectedAction.timeScale = animationControls.PlaybackSpeed;
-      // selectedAction.reset().play();
-      actionRef.current = selectedAction;
-      setIsReversing(false);
-    }
-  }, [
-    actions,
-    animationControls.Animation,
-    animationControls.PlaybackSpeed,
-    animationControls.LoopPlayback,
-  ]);
 
   useEffect(() => {
     // console.log(actionRef.current);
@@ -113,37 +50,11 @@ function Model({ url }: { url: string }) {
     });
   }, []);
 
-  useFrame(() => {
-    // if (actionRef.current) {
-    //   const currentTime = actionRef.current.time;
-    //   console.log(currentTime);
-    //   if (!isReversing && currentTime >= targetTime) {
-    //     // ターゲット時間に達したら停止してリバース
-    //     actionRef.current.fadeOut(0.1);
-    //     actionRef.current.stopFading();
-    //     // actionRef.current.timeScale = -1;
-    //     // actionRef.current.play();
-    //     setIsReversing(true);
-    //   }
-    //   // if (isReversing && currentTime <= 0) {
-    //   //   // リバース再生が終了したら停止
-    //   //   actionRef.current.stop();
-    //   //   actionRef.current.timeScale = animationControls.PlaybackSpeed;
-    //   //   setIsReversing(false);
-    //   // }
-    // }
-  });
-
   return <primitive object={scene} />;
 }
 
 export function GlbLoader() {
-  const [gameBodyUrl, setGameBodyUrl] = useState("/assets/3d/gameboy.glb");
-
-  useEffect(() => {
-    // モデルのURLを動的に設定することもできます
-    // 例: setModelUrl('https://example.com/path/to/your/model.glb')
-  }, []);
+  const [gameBodyUrl] = useState("/assets/3d/gameboy.glb");
 
   return (
     <div className="w-full h-screen">
